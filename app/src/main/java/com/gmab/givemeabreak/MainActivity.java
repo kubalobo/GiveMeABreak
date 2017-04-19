4c76a49058e757f31ba1c4648256f1bd0156ee19
Kuba <kubalobo@gmail.com>
2017-04-19 18:51:22 +0200

    Asking permissions on first start.

--- app/src/main/java/com/gmab/givemeabreak/MainActivity.java
+++ app/src/main/java/com/gmab/givemeabreak/MainActivity.java
@@ -1,8 +1,16 @@
 package com.gmab.givemeabreak;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.SQLException;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.Window;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +21,8 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkAndRequestPermissions();
 
         // Check that the activity is using the layout version with
         // the fragment_container FrameLayout
@@ -37,4 +47,37 @@ public class MainActivity extends Activity {
                    .add(R.id.fragment_container, firstFragment).commit();
         }
     }
+
+
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    /**
+     * Simple method to check multiple permissions
+     * In order to check new permission:
+     * 1. define new int (look on int storage)
+     * 2. make new if statement made like storage one
+     * @return true if permissions are granted
+     */
+    private  boolean checkAndRequestPermissions() {
+
+        int storage = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (storage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!listPermissionsNeeded.isEmpty())
+        {
+            ActivityCompat.requestPermissions(this,listPermissionsNeeded.toArray
+                    (new String[listPermissionsNeeded.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        //DataBase init
+
+    }
+
+
 }
